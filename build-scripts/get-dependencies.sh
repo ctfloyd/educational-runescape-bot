81e45b3d3aa3b262266aa63828917077d3bfb177
ctfloyd <ctfloyd@wisc.edu>
2021-08-29 16:18:19 -0500

    Update dependencies script with correct java version

--- build-scripts/get-dependencies.sh
+++ build-scripts/get-dependencies.sh
@@ -2,5 +2,5 @@
 
 wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
-sudo apt-get update; sudo apt-get install -y java-8-amazon-corretto-jdk
+sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk
 
